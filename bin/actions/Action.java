7698a743f8f201b66e7b96ba760c85300e9428ce
Chen Jiani <jianicjn04@gmail.com>
2026-03-30 11:14:59 +0800

    Push

---
+++ bin/actions/Action.java
@@ -0,0 +1,8 @@
+package actions;
+import combatants.Combatant;// import combatants
+import combat.BattleInfo;//import battle information
+
+public interface Action {
+    void execute(Combatant user, BattleInfo context);//function execute to be realized
+    String getName();//function get name to be realized
+}

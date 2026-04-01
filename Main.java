f22b37d2ea71375e3c990860b62121334a2cd963
Brayden-Choi <goldmerlin9@gmail.com>
2026-04-01 17:14:03 +0800

    Add CLI and Java ref to CLI

---
+++ Main.java
@@ -0,0 +1,10 @@
+import ui.CLI;
+import combat.BattleEngine;
+
+public class Main {
+    public static void main(String[] args) {
+        CLI cli = new CLI();
+        // GameController gc = new GameController(cli, new BattleEngine());
+        // gc.run();
+    }
+}
